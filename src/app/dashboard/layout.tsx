dfc56f6ac1be6ec8f2df661fa9c5b4060fa11def
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-21 17:03:35 -0400

    feat(layout): add new layout component for dashboard and project page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { DashBoardLayoutAppSidebar } from "@/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar";
+import DashBoardLayoutHeader from "@/sections/dashboard-layout/components/header/dashboard-layout-header";
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <DashBoardLayoutAppSidebar />
+      <SidebarInset>
+        <DashBoardLayoutHeader />
+        <div className="container bg-background mx-auto p-2">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
