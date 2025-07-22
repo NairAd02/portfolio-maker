3bb7e160a7932904fff5de9e612e28f1c56379ac
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-22 13:08:46 -0400

    feat(preview-modal): add title and icon to the Modal component in PreviewModal

--- src/components/preview-image/preview-modal.tsx
+++ src/components/preview-image/preview-modal.tsx
@@ -1,25 +1,30 @@
-"use client"
+"use client";
 
-import React, { useContext } from "react"
-import { PreviewContext } from "./context/preview-context"
-import Image from "next/image"
-import Modal from "@/components/modal/modal"
-import { modalTypes } from "@/components/modal/types/modalTypes"
+import React, { useContext } from "react";
+import { PreviewContext } from "./context/preview-context";
+import Image from "next/image";
+import Modal from "@/components/modal/modal";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import { Image as ImageIcon } from "lucide-react";
 
 export default function PreviewModal() {
-	const { preview } = useContext(PreviewContext)
-	return (
-		<Modal formPath={modalTypes.previewImageModal.name}>
-			<div className="flex items-center justify-center p-2">
-				<Image
-					src={preview}
-					alt="Testimonial author"
-					width={500}
-					height={500}
-					className="object-cover"
-					quality={100}
-				/>
-			</div>
-		</Modal>
-	)
+  const { preview } = useContext(PreviewContext);
+  return (
+    <Modal
+      title={modalTypes.previewImageModal.title}
+      icon={<ImageIcon />}
+      formPath={modalTypes.previewImageModal.name}
+    >
+      <div className="flex items-center justify-center p-2">
+        <Image
+          src={preview}
+          alt="Testimonial author"
+          width={500}
+          height={500}
+          className="object-cover"
+          quality={100}
+        />
+      </div>
+    </Modal>
+  );
 }
