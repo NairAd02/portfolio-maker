029cc31c3fa5959da271dcb5ee33f4c453d1ba05
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-22 13:58:41 -0400

    feat(proyects): add a modal to create new projects on the projects page

--- src/app/dashboard/proyects/page.tsx
+++ src/app/dashboard/proyects/page.tsx
@@ -1,8 +1,21 @@
+import Modal from "@/components/modal/modal";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import NewProjectFormModalContainer from "@/sections/proyects/form/new/new-project-form-modal-container";
 import ProyectsContainer from "@/sections/proyects/proyects-container";
 import React from "react";
 
-
-
 export default async function ProyectsPage() {
-  return <ProyectsContainer />;
+  return (
+    <>
+      <ProyectsContainer />
+      <Modal
+        formPath={modalTypes.newProyectModal.name}
+        title={modalTypes.newProyectModal.title}
+        maxWidth="max-w-3xl"
+        className="max-h-[90vh] min-h-[90vh]"
+      >
+        <NewProjectFormModalContainer />
+      </Modal>
+    </>
+  );
 }
