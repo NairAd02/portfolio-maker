d8ae4c13c09dc6a580c2ed536ae74391fab43f55
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-21 22:03:23 -0400

    feat(header-navbar): add logic to show login and register buttons only if not in the dashboard path

--- src/sections/root-layout/components/header/components/header-navbar/header-navbar.tsx
+++ src/sections/root-layout/components/header/components/header-navbar/header-navbar.tsx
@@ -1,19 +1,30 @@
+"use client";
 import NavigationComponent from "@/components/navigation-component/navigation-component";
 import { Button } from "@/components/ui/button";
 import { paths } from "@/routes/path";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function HeaderNavbar() {
+  const pathname = usePathname();
   return (
     <nav className="flex items-center gap-2 sm:gap-4">
-      <Button className="text-xs sm:text-base" variant="outline" size="sm">
-        Iniciar Sesión
-      </Button>
-      <NavigationComponent href={paths.proyects.root}>
-        <Button className="text-xs sm:text-base" size="sm" variant={"default"}>
-          Empieza Gratis
-        </Button>
-      </NavigationComponent>
+      {!pathname.includes("dashboard") && (
+        <div className="flex items-center gap-2 sm:gap-4">
+          <Button className="text-xs sm:text-base" variant="outline" size="sm">
+            Iniciar Sesión
+          </Button>
+          <NavigationComponent href={paths.proyects.root}>
+            <Button
+              className="text-xs sm:text-base"
+              size="sm"
+              variant={"default"}
+            >
+              Empieza Gratis
+            </Button>
+          </NavigationComponent>
+        </div>
+      )}
     </nav>
   );
 }
