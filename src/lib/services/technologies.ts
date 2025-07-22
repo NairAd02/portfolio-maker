b41e0df37fe8e84c0cc1cb2838558af7d914e74b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-22 18:40:31 -0400

    feat(services): add function to get list of technologies from Supabase

---
+++ src/lib/services/technologies.ts
@@ -0,0 +1,10 @@
+import { createClient } from "../supabase/server";
+
+export async function getTechnologiesList() {
+  const supabase = await createClient();
+  const { data: technologies, error } = await supabase
+    .from("technology")
+    .select("*");
+  console.log(technologies);
+  return { data: technologies, error };
+}
