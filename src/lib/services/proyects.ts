61b848041021e94f12e7ec7f67c4b4c978881c9d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-22 00:45:55 -0400

    refactor(proyects): simplify the getProyectsList function and remove unused code

--- src/lib/services/proyects.ts
+++ src/lib/services/proyects.ts
@@ -5,22 +5,6 @@ import { createClient } from "../supabase/server";
 export async function getProyectsList() {
   const supabase = await createClient();
   const { data: proyects, error } = await supabase.from("proyect").select("*");
-  console.log(error)
   console.log(proyects);
+  return { data: proyects, error };
 }
-
-export async function getProyectsList(params: IQueryable) {
-    const session = await auth();
-  
-    const url = new QueryParamsURLFactory(params, apiRoutes.configs.get).build();
-  
-    const res = await fetch(url, {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + session?.accessToken,
-      },
-      next: { tags: [tagsCacheByRoutes.configs.multipleTag] },
-    });
-  
-    return await buildApiResponse<PaginationResponse<Config>>(res);
-  }
