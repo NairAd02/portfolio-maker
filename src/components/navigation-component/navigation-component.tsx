cd6e79cd08ff168c2548fa0e61371be6674c3e86
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-21 21:55:13 -0400

    feat(navigation-component): add support for opening links in a new tab using the 'inAnotherTab' property

--- src/components/navigation-component/navigation-component.tsx
+++ src/components/navigation-component/navigation-component.tsx
@@ -1,11 +1,26 @@
-import Link from "next/link"
-import React, { ReactNode } from "react"
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
 interface Props {
-	children: ReactNode
-	href: string
+  children: ReactNode;
+  href: string;
+  inAnotherTab?: boolean;
 }
 
-export default function NavigationComponent({ children, href }: Props) {
-	return <Link href={href}>{children}</Link>
+export default function NavigationComponent({
+  children,
+  href,
+  inAnotherTab = false,
+}: Props) {
+  return (
+    <Link
+      href={href}
+      {...(inAnotherTab && {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      })}
+    >
+      {children}
+    </Link>
+  );
 }
