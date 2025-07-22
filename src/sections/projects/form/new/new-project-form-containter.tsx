fd66e35bf43b7f2ee65d24d4efa647622e53f462
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-22 17:55:24 -0400

    feat(form): implement the form container to create new projects with validation and action handling

--- src/sections/projects/form/new/new-project-form-containter.tsx
+++ src/sections/projects/form/new/new-project-form-containter.tsx
@@ -1,6 +1,65 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useContext } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { ModalContext } from "@/components/modal/context/modalContext";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import { toast } from "react-toastify";
+import useCreateProject from "../../hooks/use-create-project";
+import { paths } from "@/routes/path";
+import {
+  ProjectCreate,
+  projectCreateSchema,
+} from "./schemas/project-create-schema";
+import { revalidateServerPath } from "@/lib/cache";
+import FormActionButtons from "@/components/form/components/form-action-buttons";
 import ProjectForm from "../project-form";
 
 export default function NewProjectFormContainer() {
-  return <ProjectForm />;
+  const { handleCloseModal } = useContext(ModalContext);
+  const { loading: submitLoading, createProject } = useCreateProject({
+    onCreateAction: () => {
+      toast.success("Proyecto creado con éxito");
+      handleClose();
+      revalidateServerPath(paths.projects.root);
+    },
+  });
+  const form = useForm<ProjectCreate>({
+    resolver: zodResolver(projectCreateSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      deploymentUrl: "",
+      impact: "",
+      problem: "",
+      solution: "",
+      sourceCodeUrl: "",
+      teachings: "",
+      technologies: [],
+      images: [],
+    },
+  });
+
+  const handleClose = () => {
+    handleCloseModal(modalTypes.newProyectModal.name);
+  };
+
+  function onSubmit(project: ProjectCreate) {
+    createProject(project);
+  }
+  return (
+    <FormProvider {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
+      >
+        <ProjectForm />
+        <FormActionButtons
+          submitLoading={submitLoading}
+          submitButtonText="Crear Proyecto"
+          handleClose={handleClose}
+        />
+      </form>
+    </FormProvider>
+  );
 }
