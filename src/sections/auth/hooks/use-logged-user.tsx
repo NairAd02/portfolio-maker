48d2fad24b42c7bdd4b7241b7ca7551aea7ca715
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-23 16:41:40 -0400

    feat(auth): add hook to get the authenticated user

---
+++ src/sections/auth/hooks/use-logged-user.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { getLoggedUser as getLoggedUserService } from "@/lib/services/auth";
+import { User } from "@supabase/supabase-js";
+import { useCallback, useEffect, useState } from "react";
+
+export default function useLoggedUser() {
+  const [loggedUser, setLoggedUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const fethLoggedUser = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    const res = await getLoggedUserService();
+    if (!res.data || res.error) {
+      console.log(res.error);
+      setError("Error en la operación de obtención de sessión");
+    } else setLoggedUser(res.data.user);
+  }, []);
+
+  useEffect(() => {
+    fethLoggedUser();
+  }, [fethLoggedUser]);
+
+  return { loggedUser, loading, error, fethLoggedUser };
+}
