f675c8ee53be1009734d1f5a91424f123b867dac
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-23 13:40:52 -0400

    feat(auth): add user verification to the dashboard layout and redirect to the login page if not authenticated.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { DashBoardLayoutAppSidebar } from "@/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar";
 import DashBoardLayoutHeader from "@/sections/dashboard-layout/components/header/dashboard-layout-header";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { paths } from "@/routes/path";
 
-export default function layout({
+export default async function layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data?.user) {
+    redirect(paths.sign_in.root);
+  }
   return (
     <SidebarProvider>
       <DashBoardLayoutAppSidebar />
