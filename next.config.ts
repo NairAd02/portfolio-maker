27d17e3e15dd1602031f8967ed2b94651838c794
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-22 22:28:47 -0400

    feat(config): add remote pattern configuration for images from Supabase in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kulraeahqplmequlfsjo.supabase.co",
+        pathname: "/storage/v1/object/public/portfolio-maker/**", // bucket name
+      },
+    ],
+  },
 };
 
 export default nextConfig;
