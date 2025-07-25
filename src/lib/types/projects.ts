52d886d8be8c9570ed4436b1ce202e2e3da80e73
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-25 00:04:52 -0400

    feat(projects): add ProjectEditDTO interface and function to convert project edit data

--- src/lib/types/projects.ts
+++ src/lib/types/projects.ts
@@ -1,5 +1,6 @@
 import { ProjectCreate } from "@/sections/projects/form/new/schemas/project-create-schema";
 import { Technology } from "./technologies";
+import { ProjectEdit } from "@/sections/projects/form/edit/schemas/project-edit-schema";
 
 export interface Project {
   id: string;
@@ -27,7 +28,21 @@ export interface ProjectCreateDTO {
   solution: string;
   impact: string;
   teachings: string;
-  portfolio_id: string
+  portfolio_id: string;
+  technologies: string[];
+}
+
+export interface ProjectEditDTO {
+  name: string;
+  description: string;
+  mainImage?: string;
+  sourceCodeUrl?: string;
+  deploymentUrl?: string;
+  images: string[];
+  problem: string;
+  solution: string;
+  impact: string;
+  teachings: string;
   technologies: string[];
 }
 
@@ -39,6 +54,17 @@ export const convertProjectCreateDTO = (
     images: [],
     sourceCodeUrl: projectCreate.sourceCodeUrl || undefined,
     deploymentUrl: projectCreate.deploymentUrl || undefined,
-    portfolio_id: ""
+    portfolio_id: "",
+  };
+};
+
+export const convertProjectEditDTO = (
+  projectEdit: Omit<ProjectEdit, "images" | "mainImage">
+): ProjectEditDTO => {
+  return {
+    ...projectEdit,
+    images: [],
+    sourceCodeUrl: projectEdit.sourceCodeUrl || undefined,
+    deploymentUrl: projectEdit.deploymentUrl || undefined,
   };
 };
