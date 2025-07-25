db4ef5bd23adbf5a3c7c16fd8c54093f3e507890
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-25 00:09:51 -0400

    feat(projects): implement the useEditProject hook to edit projects and handle image loading

---
+++ src/sections/projects/hooks/use-edit-project.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useCallback, useState } from "react";
+import { ProjectEdit } from "../form/edit/schemas/project-edit-schema";
+import { editProject as editProjectService } from "@/lib/services/projects";
+import { convertProjectEditDTO } from "@/lib/types/projects";
+
+interface Props {
+  id: string;
+  onEditAction: () => void;
+}
+
+export default function useEditProject({ id, onEditAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const editProject = useCallback(
+    async (project: ProjectEdit) => {
+      const { mainImage, images, ...restProject } = project;
+
+      setLoading(true);
+      setError(null);
+      // create form data for image
+      const formData = new FormData();
+      if (mainImage) formData.append("mainImage", mainImage);
+      images.forEach((image) => {
+        formData.append("images[]", image);
+      });
+
+      const res = await editProjectService(
+        id,
+        convertProjectEditDTO(restProject),
+        formData
+      );
+
+      if (res.error) setError("Error en la edición del proyecto");
+      else {
+        onEditAction();
+      }
+
+      setLoading(false);
+    },
+    [onEditAction, id]
+  );
+  return {
+    loading,
+    error,
+    editProject,
+  };
+}
