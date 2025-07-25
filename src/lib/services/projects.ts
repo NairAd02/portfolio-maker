438bec8b32cae4dc24f720213c752064c390762a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-24 23:39:46 -0400

    refactor(projects): move instert functions to end

--- src/lib/services/projects.ts
+++ src/lib/services/projects.ts
@@ -7,7 +7,6 @@ import { getLoggedUser } from "./auth";
 import { getImageUrlOrThrow } from "./supabase-storage";
 import { uploadFileToSupabase } from "./supabase-storage";
 
-
 export async function getProjectsList() {
   const supabase = await createClient();
   const { data, error } = await supabase.from("project").select("*");
@@ -44,6 +43,66 @@ export async function getProjectsList() {
     return { data: null, error: err };
   }
 }
+
+export async function createProject(
+  projectCreateDTO: ProjectCreateDTO,
+  formData: FormData
+) {
+  const { technologies, ...restProjectCreateDTO } = projectCreateDTO;
+  const supabase = await createClient();
+
+  // find the portfolio user
+  const { data: imagesData, error: insertImagesError } =
+    await insertProjectImages(supabase, restProjectCreateDTO.name, formData);
+
+  if (!imagesData || insertImagesError)
+    return { data: null, error: insertImagesError };
+
+  // get the session
+  const { data: sessionData, error: loggedUserError } = await getLoggedUser();
+
+  if (!sessionData || loggedUserError) return { data: null, loggedUserError };
+
+  const { data: portfolio, error: portfolioError } = await supabase
+    .from("portfolio")
+    .select("id")
+    .eq("user_id", sessionData.user.id)
+    .single();
+
+  if (portfolioError) return { data: null, error: portfolioError };
+
+  // Preparar el objeto para insertar
+  const insertData = {
+    ...restProjectCreateDTO,
+    mainImage: imagesData.mainImage,
+    images: imagesData.images,
+    portfolio_id: portfolio.id,
+  };
+
+  const { data, error: projectError } = await supabase
+    .from("project")
+    .insert(insertData)
+    .select()
+    .single();
+  if (projectError) return { data: null, error: projectError };
+
+  const projectEntity = data as Project;
+
+  // Insertar tecnologías relacionadas
+  if (technologies && technologies.length > 0) {
+    const { error: technologiesError } = await insertProjectTechnologies(
+      supabase,
+      projectEntity.id,
+      technologies
+    );
+
+    if (technologiesError) return { data: null, error: technologiesError };
+  }
+
+  return { data: projectEntity, error: null };
+}
+
+// functions auxs
 
 async function insertProjectImages(
   supabase: SupabaseClient<any, "public", any>,
@@ -116,62 +175,4 @@ async function insertProjectTechnologies(
   if (technologiesError) return { data: null, error: technologiesError };
 
   return { data, error: null };
-}
-
-export async function createProject(
-  projectCreateDTO: ProjectCreateDTO,
-  formData: FormData
-) {
-  const { technologies, ...restProjectCreateDTO } = projectCreateDTO;
-  const supabase = await createClient();
-
-  // find the portfolio user
-  const { data: imagesData, error: insertImagesError } =
-    await insertProjectImages(supabase, restProjectCreateDTO.name, formData);
-
-  if (!imagesData || insertImagesError)
-    return { data: null, error: insertImagesError };
-
-  // get the session
-  const { data: sessionData, error: loggedUserError } = await getLoggedUser();
-
-  if (!sessionData || loggedUserError) return { data: null, loggedUserError };
-
-  const { data: portfolio, error: portfolioError } = await supabase
-    .from("portfolio")
-    .select("id")
-    .eq("user_id", sessionData.user.id)
-    .single();
-
-  if (portfolioError) return { data: null, error: portfolioError };
-
-  // Preparar el objeto para insertar
-  const insertData = {
-    ...restProjectCreateDTO,
-    mainImage: imagesData.mainImage,
-    images: imagesData.images,
-    portfolio_id: portfolio.id,
-  };
-
-  const { data, error: projectError } = await supabase
-    .from("project")
-    .insert(insertData)
-    .select()
-    .single();
-  if (projectError) return { data: null, error: projectError };
-
-  const projectEntity = data as Project;
-
-  // Insertar tecnologías relacionadas
-  if (technologies && technologies.length > 0) {
-    const { error: technologiesError } = await insertProjectTechnologies(
-      supabase,
-      projectEntity.id,
-      technologies
-    );
-
-    if (technologiesError) return { data: null, error: technologiesError };
-  }
-
-  return { data: projectEntity, error: null };
 }
