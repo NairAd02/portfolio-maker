6c433e69a5b3e50e0e5460e8ff90b0fe3d9fed0c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-07 11:18:03 -0400

    feat(delete-project): add page and container for project deletion with confirmation

---
+++ src/sections/projects/delete/project-delete-container.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { useCallback } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { paths } from "@/routes/path";
+import { revalidateServerPath } from "@/lib/cache";
+import useDeleteProject from "../hooks/use-delete-project";
+import ConfirmationPanel from "@/components/confirmation-panel/confirmation-panel";
+
+interface Props {
+  id: string;
+}
+
+export default function ProjectDeleteContainer({ id }: Props) {
+  const router = useRouter();
+  const { deleteProject, loading } = useDeleteProject({
+    id,
+    onDeleteAction: () => {
+      toast.success("Proyecto eliminado con éxito");
+      revalidateServerPath(paths.projects.root);
+      handleClose();
+      router.refresh();
+    },
+  });
+  const handleClose = useCallback(() => {
+    router.back();
+  }, [router]);
+  return (
+    <ConfirmationPanel
+      title={"Eliminación del Proyecto"}
+      message={"¿Seguro que desea eliminar el proyecto?"}
+      warningMessage={
+        "La eliminación del proyecto es irreversible, este cambio no se puede deshacer"
+      }
+      onConfirm={deleteProject}
+      onCancel={handleClose}
+      isLoading={loading}
+      isDestructive
+    />
+  );
+}
