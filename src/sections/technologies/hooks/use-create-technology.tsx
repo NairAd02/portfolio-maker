24730ced83df401cc0c6e257095f3cdfab5718d2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-07 18:41:08 -0400

    feat(technologies): implement hooks to create technology with loading state and error handling

---
+++ src/sections/technologies/hooks/use-create-technology.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useCallback, useState } from "react";
+import { convertTechnologyCreateDTO } from "@/lib/types/technologies";
+import { TechnologyCreate } from "../form/schemas/technology-create-schema";
+import { createTechnology as createTechnologyService } from "@/lib/services/technologies";
+
+interface Props {
+  onCreateAction: () => void;
+}
+
+export default function useCreateTechnology({ onCreateAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const createTechnology = useCallback(
+    async (technology: TechnologyCreate) => {
+      setLoading(true);
+      setError(null);
+
+      const { icon, ...restTechnology } = technology;
+
+      // create form data for image
+      const formData = new FormData();
+      if (icon) formData.append("icon", icon);
+
+      const res = await createTechnologyService(
+        convertTechnologyCreateDTO(restTechnology),
+        formData
+      );
+
+      if (res.error) {
+        console.log(res.error);
+        setError("Error en la creación de la tecnología");
+      } else {
+        onCreateAction();
+      }
+
+      setLoading(false);
+    },
+    [onCreateAction]
+  );
+  return {
+    loading,
+    error,
+    createTechnology,
+  };
+}
