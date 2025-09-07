bd60e8043e3cf21f3954b16dd4e576f7bfa41129
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-06 22:41:25 -0400

    refactor(details-section-header): remove the href property and use the Next.js router for navigation on return.

--- src/components/details-section-header/details-section-header.tsx
+++ src/components/details-section-header/details-section-header.tsx
@@ -1,18 +1,14 @@
-import NavigationComponent from "../navigation-component/navigation-component";
 import { ArrowLeft } from "lucide-react";
 import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
 interface Props {
-  href: string;
   title: string;
   description: string;
 }
 
-export default function DetailsSectionHeader({
-  href,
-  title,
-  description,
-}: Props) {
+export default function DetailsSectionHeader({ title, description }: Props) {
+  const router = useRouter();
   return (
     <div className="relative flex flex-col sm:flex-row sm:items-center bg-white rounded-lg shadow-sm gap-4 p-6 overflow-hidden">
       {/* Panel azul diagonal superpuesto */}
@@ -25,16 +21,18 @@ export default function DetailsSectionHeader({
 
       {/* Contenido principal con z-index para estar por encima */}
       <div className="relative z-10 flex flex-col sm:flex-row sm:items-center gap-4 w-full">
-        <NavigationComponent href={href}>
-          <Button
-            variant="outline"
-            size="sm"
-            className="gap-2 bg-white text-black border-gray-300 hover:bg-gray-50"
-          >
-            <ArrowLeft className="w-4 h-4" />
-            Volver
-          </Button>
-        </NavigationComponent>
+        <Button
+          variant="outline"
+          size="sm"
+          className="gap-2 bg-white text-black border-gray-300 hover:bg-gray-50"
+          onClick={() => {
+            router.back();
+          }}
+        >
+          <ArrowLeft className="w-4 h-4" />
+          Volver
+        </Button>
+
         <div className="flex-1">
           <h1 className="text-2xl font-bold text-gray-900">{title}</h1>
           <p className="text-gray-600">{description}</p>
