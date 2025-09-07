f92590570f85bdaa899c666228f0e8f12658bc7e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-07 16:08:35 -0400

    feat(projects-filters): replace search fields with text input components and adjust the layout

--- src/sections/projects/filters/projects-filters.tsx
+++ src/sections/projects/filters/projects-filters.tsx
@@ -5,6 +5,7 @@ import ListInput from "@/components/inputs/list-input/list-input";
 import { OptionData } from "@/lib/types/filters";
 import { ProjectsFilters as ProjectsFiltersType } from "./hooks/use-projects-filters";
 import { Technology } from "@/lib/types/technologies";
+import TextareaInput from "@/components/inputs/textarea-input/textarea-input";
 
 interface Props {
   filters: ProjectsFiltersType;
@@ -32,71 +33,8 @@ export default function ProjectsFilters({
   );
 
   return (
-    <div className="space-y-6 p-4">
-      {/* Search for name */}
-      <div className="flex gap-4 items-center">
-        <SearchInput
-          id="name"
-          label="Buscar por nombre"
-          value={filters.name}
-          placeHolder="Introduzca el nombre..."
-          onChange={(e) => {
-            handleChangeFilters({ name: e.target.value || undefined });
-          }}
-        />
-        <SearchInput
-          id="description"
-          label="Buscar por descripción"
-          value={filters.description}
-          placeHolder="Introduzca la descripción..."
-          onChange={(e) => {
-            handleChangeFilters({ description: e.target.value || undefined });
-          }}
-        />
-      </div>
-
-      <SearchInput
-        id="problem"
-        label="Buscar por problema"
-        value={filters.problem}
-        placeHolder="Introduzca el problema del proyecto..."
-        onChange={(e) => {
-          handleChangeFilters({ problem: e.target.value || undefined });
-        }}
-      />
-
-      <SearchInput
-        id="impact"
-        label="Buscar por impacto"
-        value={filters.impact}
-        placeHolder="Introduzca el impacto del proyecto..."
-        onChange={(e) => {
-          handleChangeFilters({ impact: e.target.value || undefined });
-        }}
-      />
-
-      <SearchInput
-        id="impact"
-        label="Buscar por solución"
-        value={filters.solution}
-        placeHolder="Introduzca la solución del proyecto..."
-        onChange={(e) => {
-          handleChangeFilters({ solution: e.target.value || undefined });
-        }}
-      />
-
-      <SearchInput
-        id="impact"
-        label="Buscar por Aprendizaje"
-        value={filters.teachings}
-        placeHolder="Introduzca el aprendizaje del proyecto..."
-        onChange={(e) => {
-          handleChangeFilters({ teachings: e.target.value || undefined });
-        }}
-      />
-
-      <Separator />
-      {/* Scents */}
+    <div className="space-y-6 p-4 overflow-auto">
+      {/* Technologies */}
       <ListInput
         id="technologies"
         label="Tecnologías"
@@ -107,6 +45,66 @@ export default function ProjectsFilters({
         }))}
         loading={technologies.loading}
         handleValuesChange={handleTechnologiesChange}
+      />
+      <Separator className="bg-primary" />
+      {/* Search for name */}
+      <SearchInput
+        id="name"
+        label="Buscar por nombre"
+        value={filters.name}
+        placeHolder="Introduzca el nombre..."
+        onChange={(e) => {
+          handleChangeFilters({ name: e.target.value || undefined });
+        }}
+      />
+      <TextareaInput
+        id="description"
+        label="Buscar por descripción"
+        value={filters.description}
+        placeHolder="Introduzca la descripción..."
+        onChange={(e) => {
+          handleChangeFilters({ description: e.target.value || undefined });
+        }}
+      />
+
+      <TextareaInput
+        id="problem"
+        label="Buscar por problema"
+        value={filters.problem}
+        placeHolder="Introduzca el problema del proyecto..."
+        onChange={(e) => {
+          handleChangeFilters({ problem: e.target.value || undefined });
+        }}
+      />
+
+      <TextareaInput
+        id="impact"
+        label="Buscar por impacto"
+        value={filters.impact}
+        placeHolder="Introduzca el impacto del proyecto..."
+        onChange={(e) => {
+          handleChangeFilters({ impact: e.target.value || undefined });
+        }}
+      />
+
+      <TextareaInput
+        id="solution"
+        label="Buscar por solución"
+        value={filters.solution}
+        placeHolder="Introduzca la solución del proyecto..."
+        onChange={(e) => {
+          handleChangeFilters({ solution: e.target.value || undefined });
+        }}
+      />
+
+      <TextareaInput
+        id="teachings"
+        label="Buscar por Aprendizaje"
+        value={filters.teachings}
+        placeHolder="Introduzca el aprendizaje del proyecto..."
+        onChange={(e) => {
+          handleChangeFilters({ teachings: e.target.value || undefined });
+        }}
       />
     </div>
   );
