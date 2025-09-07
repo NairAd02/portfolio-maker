d5b25d777c093fc00145f9581073cf055bd5663e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-07 18:40:02 -0400

    feat(technologies): add TechnologyCreateDTO interface and function to convert technology creation data

--- src/lib/types/technologies.ts
+++ src/lib/types/technologies.ts
@@ -1,6 +1,20 @@
+import { TechnologyCreate } from "@/sections/technologies/form/schemas/technology-create-schema";
+
 export interface Technology {
   id: string;
   name: string;
   icon?: string;
   color: string;
 }
+
+export interface TechnologyCreateDTO {
+  name: string;
+}
+
+export const convertTechnologyCreateDTO = (
+  technology: Omit<TechnologyCreate, "icon">
+): TechnologyCreateDTO => {
+  return {
+    ...technology,
+  };
+};
