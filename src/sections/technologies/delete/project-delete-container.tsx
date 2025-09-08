1710dd7fadd84e5afc2e91e2f70b46b2212424cf
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-07 22:45:51 -0400

    feat(technologies): add container for technology removal with confirmation and loading status management

---
+++ src/sections/technologies/delete/project-delete-container.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { useCallback } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { paths } from "@/routes/path";
+import { revalidateServerPath } from "@/lib/cache";
+import ConfirmationPanel from "@/components/confirmation-panel/confirmation-panel";
+import useDeleteTechnology from "../hooks/use-delete-technology";
+
+interface Props {
+  id: string;
+}
+
+export default function TechnologyDeleteContainer({ id }: Props) {
+  const router = useRouter();
+  const { deleteTechnology, loading } = useDeleteTechnology({
+    id,
+    onDeleteAction: () => {
+      toast.success("Tecnología eliminada con éxito");
+      revalidateServerPath(paths.technologies.root);
+      handleClose();
+      router.refresh();
+    },
+  });
+  const handleClose = useCallback(() => {
+    router.back();
+  }, [router]);
+  return (
+    <ConfirmationPanel
+      title={"Eliminación de la Tecnología"}
+      message={"¿Seguro que desea eliminar la tecnología?"}
+      warningMessage={
+        "La eliminación de la tecnología es irreversible, este cambio no se puede deshacer"
+      }
+      onConfirm={deleteTechnology}
+      onCancel={handleClose}
+      isLoading={loading}
+      isDestructive
+    />
+  );
+}
