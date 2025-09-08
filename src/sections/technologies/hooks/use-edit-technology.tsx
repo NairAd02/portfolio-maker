6207e8b4045678ad4503d811e5b57fd9e32113ea
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-07 21:23:28 -0400

    feat(technologies): add useEditTechnology hook to manage technology editing with loading status and error handling

---
+++ src/sections/technologies/hooks/use-edit-technology.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useCallback, useState } from "react";
+import { TechnologyEdit } from "../form/edit/schemas/technology-edit-schema";
+import { editTechnology as editTechnologyService } from "@/lib/services/technologies";
+import { convertTechnologyEditDTO } from "@/lib/types/technologies";
+
+interface Props {
+  id: string;
+  onEditAction: () => void;
+}
+
+export default function useEditTechnology({ id, onEditAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const editTechnology = useCallback(
+    async (technology: TechnologyEdit) => {
+      const { icon, ...restProject } = technology;
+
+      setLoading(true);
+      setError(null);
+      // create form data for icon
+      const formData = new FormData();
+      if (icon) formData.append("icon", icon);
+
+      const res = await editTechnologyService(
+        id,
+        convertTechnologyEditDTO(restProject),
+        formData
+      );
+
+      if (res.error) setError("Error en la edición de la tecnología");
+      else {
+        onEditAction();
+      }
+
+      setLoading(false);
+    },
+    [onEditAction, id]
+  );
+  return {
+    loading,
+    error,
+    editTechnology,
+  };
+}
