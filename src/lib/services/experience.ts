9ec7ec3c98c5619d5d0e42edde994cd191497c4f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-09 11:53:38 -0400

    feat(experiences):  add function to get list of experiences with image handling

---
+++ src/lib/services/experience.ts
@@ -0,0 +1,28 @@
+"use server";
+import { createClient } from "../supabase/server";
+import { Experience } from "../types/experience";
+import { getImageUrlOrThrow } from "./supabase-storage";
+
+export async function getExperiencesList() {
+  const supabase = await createClient();
+  const { data: experiencesData, error } = await supabase
+    .from("experience")
+    .select("*");
+  const experiences = experiencesData as Experience[];
+
+  try {
+    const experiencesMapped = await Promise.all(
+      experiences.map(async (experience) => {
+        return {
+          ...experience,
+          mainImage: experience.mainImage
+            ? await getImageUrlOrThrow(supabase, experience.mainImage)
+            : undefined,
+        };
+      })
+    );
+    return { data: experiencesMapped, error };
+  } catch (error) {
+    return { data: null, error };
+  }
+}
