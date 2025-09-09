304ad8f496299148469a48cd39e10adb23402f63
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-09 16:18:57 -0400

    feat(experiences): add DTO and function to convert experience creation data

--- src/lib/types/experiences.ts
+++ src/lib/types/experiences.ts
@@ -1,3 +1,4 @@
+import { ExperienceCreate } from "@/sections/experiences/form/create/schemas/experience-create-schema";
 import { Technology } from "./technologies";
 
 export interface Experience {
@@ -21,3 +22,26 @@ export interface ExperienceDetails {
   achievements: string[];
   technologies: Technology[];
 }
+
+export interface ExperienceCreateDTO {
+  company: string;
+  position: string;
+  startdate: string;
+  enddate: string;
+  description: string;
+  achievements: string[];
+  technologies: string[];
+}
+
+export const convertExperienceCreateDTO = (
+  experience: Omit<ExperienceCreate, "mainImage">
+): ExperienceCreateDTO => {
+  return {
+    ...experience,
+    achievements: experience.achievements.map(
+      (achievement) => achievement.name
+    ),
+    startdate: experience.startdate.toISOString(),
+    enddate: experience.enddate.toISOString(),
+  };
+};
