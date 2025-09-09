234eeadca1175f406d6845d8ccb07afc7938fbb5
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-09 12:29:18 -0400

    feat(experiences): add functionality to create experiences with image management and technologies

--- src/lib/services/experiences.ts
+++ src/lib/services/experiences.ts
@@ -1,7 +1,11 @@
 "use server";
+import { SupabaseClient } from "@supabase/supabase-js";
 import { createClient } from "../supabase/server";
-import { Experience } from "../types/experiences";
-import { getImageUrlOrThrow } from "./supabase-storage";
+import { Experience, ExperienceCreateDTO } from "../types/experiences";
+import { getImageUrlOrThrow, uploadFileToSupabase } from "./supabase-storage";
+import { generateStorageFilePath } from "../images";
+import { insertExperienceTechnologies } from "./technologies";
+import { getLoggedUser } from "./auth";
 
 export async function getExperiencesList() {
   const supabase = await createClient();
@@ -25,4 +29,90 @@ export async function getExperiencesList() {
   } catch (error) {
     return { data: null, error };
   }
+}
+
+export async function createExperience(
+  experienceCreateDTO: ExperienceCreateDTO,
+  formData: FormData
+) {
+  const { technologies, ...restExperienceCreateDTO } = experienceCreateDTO;
+  const supabase = await createClient();
+
+  // get the session
+  const { data: sessionData, error: loggedUserError } = await getLoggedUser();
+
+  if (!sessionData || loggedUserError) return { data: null, loggedUserError };
+
+  // find the user portfolio
+  const { data: portfolio, error: portfolioError } = await supabase
+    .from("portfolio")
+    .select("id")
+    .eq("user_id", sessionData.user.id)
+    .single();
+
+  if (portfolioError) return { data: null, error: portfolioError };
+
+  // insert the main image
+  const { data: mainImageUploadData, error: mainImageUploadError } =
+    await insertExperienceMainImage(
+      supabase,
+      formData,
+      experienceCreateDTO.company
+    );
+
+  if (mainImageUploadError) return { data: null, error: mainImageUploadError };
+
+  const { data: createExperienceData, error: createExperienceError } =
+    await supabase
+      .from("experience")
+      .insert({
+        ...restExperienceCreateDTO,
+        mainImage: mainImageUploadData,
+        portfolio_id: portfolio.id,
+      })
+      .select()
+      .single();
+
+  if (createExperienceError)
+    return { data: null, error: createExperienceError };
+
+  // Insertar tecnologías relacionadas
+  if (technologies && technologies.length > 0) {
+    const { error: technologiesError } = await insertExperienceTechnologies(
+      supabase,
+      createExperienceData.id,
+      technologies
+    );
+
+    if (technologiesError) return { data: null, error: technologiesError };
+  }
+
+  return { data: createExperienceData, error: null };
+}
+
+// aux functions
+async function insertExperienceMainImage(
+  supabase: SupabaseClient<any, "public", any>,
+  formData: FormData,
+  experienceName: string
+) {
+  const icon = formData.get("icon") as File;
+  if (!icon) return { data: null, error: null };
+
+  const iconPath = generateStorageFilePath(
+    icon,
+    `experiences/${experienceName}/mainImage`
+  );
+
+  const uploadIconError = await uploadFileToSupabase(
+    supabase,
+    "portfolio-maker",
+    iconPath,
+    icon,
+    "3600",
+    false
+  );
+  if (uploadIconError) return { data: null, error: uploadIconError };
+
+  return { data: iconPath, error: null };
 }
