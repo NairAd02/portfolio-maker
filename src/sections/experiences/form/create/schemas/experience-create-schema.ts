4b161243a2e3b64726fb24e92dea8bf1851243b4
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-09 13:14:51 -0400

    feat(experiences): create a validation scheme for creating work experiences

---
+++ src/sections/experiences/form/create/schemas/experience-create-schema.ts
@@ -0,0 +1,66 @@
+import { z } from "zod";
+
+export interface ExperienceCreate {
+  company: string;
+  position: string;
+  startdate: string;
+  enddate: string;
+  description: string;
+  achievements: { name: string }[];
+  mainImage?: File;
+  technologies: string[];
+}
+
+export const experienceCreateSchema = z
+  .object({
+    company: z.string().min(1, { message: "El campo es requerido" }),
+    position: z.string().min(1, { message: "El campo es requerido" }),
+    description: z.string().min(1, { message: "El campo es requerido" }),
+    achievements: z.array(
+      z.object({
+        name: z.string().min(1, { message: "El campo es requerido" }),
+      })
+    ),
+    mainImage: z
+      .instanceof(File, {
+        message: "Por favor selecciona una imagen.",
+      })
+      .optional()
+      .refine(
+        (file) => !file || file.size <= 5 * 1024 * 1024,
+        "La imagen no debe exceder 5MB."
+      )
+      .refine(
+        (file) => !file || file.type.startsWith("image/"),
+        "El archivo debe ser una imagen."
+      ),
+    technologies: z.array(z.string()).min(1, {
+      message:
+        "Debes seleccionar al menos una tecnología que haya usado en durante esa experiencia laboral",
+    }),
+    startdate: z
+      .date()
+      .refine((date) => date <= new Date(), {
+        message: "La fecha no puede ser futura",
+      })
+      .refine((date) => date >= new Date("1900-01-01"), {
+        message: "La fecha es demasiado antigua",
+      }),
+    enddate: z
+      .date()
+      .refine((date) => date <= new Date(), {
+        message: "La fecha no puede ser futura",
+      })
+      .refine((date) => date >= new Date("1900-01-01"), {
+        message: "La fecha es demasiado antigua",
+      }),
+  })
+  .refine(
+    (data) => {
+      return data.startdate <= data.enddate;
+    },
+    {
+      message: "La fecha de inicio no puede ser mayor que la fecha de fin",
+      path: ["startdate"],
+    }
+  );
