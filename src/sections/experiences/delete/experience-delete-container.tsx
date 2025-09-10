b70020deaf8a1696c3a24c8dd7bfa8de007fa3ca
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-10 16:32:21 -0400

    feat(experiences): create a page to delete experiences, including confirmation and error handling

---
+++ src/sections/experiences/delete/experience-delete-container.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React, { useCallback } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { paths } from "@/routes/path";
+import { revalidateServerPath } from "@/lib/cache";
+import ConfirmationPanel from "@/components/confirmation-panel/confirmation-panel";
+import useDeleteExperience from "../hooks/use-delete-experience";
+
+interface Props {
+  id: string;
+}
+
+export default function ExperienceDeleteContainer({ id }: Props) {
+  const router = useRouter();
+  const { deleteExperience, loading, error } = useDeleteExperience({
+    id,
+    onDeleteAction: () => {
+      toast.success("Experiencia Laboral eliminada con éxito");
+      revalidateServerPath(paths.experiences.root);
+      handleClose();
+      router.refresh();
+    },
+  });
+  const handleClose = useCallback(() => {
+    router.back();
+  }, [router]);
+  return (
+    <ConfirmationPanel
+      title={"Eliminación de Experiencia Laboral"}
+      message={"¿Seguro que desea eliminar la experiencia laboral?"}
+      warningMessage={
+        "La eliminación de la experiencia laboral es irreversible, este cambio no se puede deshacer"
+      }
+      onConfirm={deleteExperience}
+      onCancel={handleClose}
+      isLoading={loading}
+      isDestructive
+      error={error}
+    />
+  );
+}
