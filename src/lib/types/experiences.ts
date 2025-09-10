6126b2939b2c0c883137e10dacd0389b9c7bbb8d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-09 20:50:37 -0400

    feat(experiences): add interface and functionality for editing experiences, including achievement and date management

--- src/lib/types/experiences.ts
+++ src/lib/types/experiences.ts
@@ -1,5 +1,6 @@
 import { ExperienceCreate } from "@/sections/experiences/form/create/schemas/experience-create-schema";
 import { Technology } from "./technologies";
+import { ExperienceEdit } from "@/sections/experiences/form/edit/schemas/experience-edit-schema";
 
 export interface Experience {
   id: string;
@@ -33,9 +34,32 @@ export interface ExperienceCreateDTO {
   technologies: string[];
 }
 
+export interface ExperienceEditDTO {
+  company: string;
+  position: string;
+  startdate: string;
+  enddate: string;
+  description: string;
+  achievements: string[];
+  technologies: string[];
+}
+
 export const convertExperienceCreateDTO = (
   experience: Omit<ExperienceCreate, "mainImage">
 ): ExperienceCreateDTO => {
+  return {
+    ...experience,
+    achievements: experience.achievements.map(
+      (achievement) => achievement.name
+    ),
+    startdate: experience.startdate.toISOString(),
+    enddate: experience.enddate.toISOString(),
+  };
+};
+
+export const convertExperienceEditDTO = (
+  experience: Omit<ExperienceEdit, "mainImage">
+): ExperienceEditDTO => {
   return {
     ...experience,
     achievements: experience.achievements.map(
