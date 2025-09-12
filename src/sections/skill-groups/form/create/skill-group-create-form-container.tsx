1bbb20ef74b1a47d32931ded5dcb2e8b9df3a1fd
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-12 11:07:40 -0400

    feat(skill-groups): add a form for creating skill groups with icon and skill uploads

---
+++ src/sections/skill-groups/form/create/skill-group-create-form-container.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { paths } from "@/routes/path";
+import { revalidateServerPath } from "@/lib/cache";
+import FormActionButtons from "@/components/form/components/form-action-buttons";
+import { useRouter } from "next/navigation";
+import { AlertDestructive } from "@/components/ui/alert-destructive";
+import useCreateSkillGroup from "../../hooks/use-create-skill-group";
+import {
+  SkillGroupCreate,
+  skillGroupCreateSchema,
+} from "./schemas/skill-group-create-schema";
+import SkillGroupForm from "../skill-group-form";
+
+export default function SkillGroupCreateFormContainer() {
+  const router = useRouter();
+  const {
+    loading: submitLoading,
+    createSkillGroup,
+    error: createSkillGroupError,
+  } = useCreateSkillGroup({
+    onCreateAction: () => {
+      toast.success("Grupo de habilidad creado con éxito");
+      revalidateServerPath(paths.skillGroups.root);
+      handleClose();
+      router.refresh();
+    },
+  });
+  const form = useForm<SkillGroupCreate>({
+    resolver: zodResolver(skillGroupCreateSchema),
+    defaultValues: {
+      name: "",
+      skills: [],
+    },
+  });
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  function onSubmit(skillGroup: SkillGroupCreate) {
+    createSkillGroup(skillGroup);
+  }
+  return (
+    <FormProvider {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
+      >
+        {createSkillGroupError && (
+          <AlertDestructive title={createSkillGroupError} />
+        )}
+        <SkillGroupForm />
+        <FormActionButtons
+          submitLoading={submitLoading}
+          submitButtonText="Crear Grupo de Habilidades"
+          handleClose={handleClose}
+        />
+      </form>
+    </FormProvider>
+  );
+}
---
+++ src/sections/skill-groups/form/skill-group-form.tsx
@@ -0,0 +1,64 @@
+import { RHFImageUpload } from "@/components/form/rhf-components/rhf-image-upload/rhf-image-upload";
+import { RHFListField } from "@/components/form/rhf-components/rhf-list-field/rhf-list-field";
+import { RHFTextField } from "@/components/form/rhf-components/rhf-text-field/rhf-text-field";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Aperture, FileTextIcon } from "lucide-react";
+import React from "react";
+import { SkillCreate } from "./create/schemas/skill-create-schema";
+import SkillGroupSkillStack from "./stacks/skill-group-skill-stack";
+import { LevelEnum } from "@/lib/types/skill-groups";
+
+interface Props {
+  imageRecived?: { loading: boolean; error: string | null };
+}
+
+export default function SkillGroupForm({ imageRecived }: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          <FileTextIcon className="w-5 h-5" />
+          Información General
+        </CardTitle>
+        <CardDescription>Información del Grupo de Habilidad</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div className="flex items-center gap-8">
+          <RHFImageUpload
+            name="icon"
+            label="Icono del Grupo de Habilidades"
+            variant="avatar"
+            avatarIcon={
+              <Aperture
+                className="text-secondary dark:text-secondary"
+                size={120 * 0.4}
+              />
+            }
+            {...(imageRecived && { loading: imageRecived.loading })}
+          />
+          <RHFTextField
+            name="name"
+            label="Nombre del Grupo de Habilidad *"
+            placeholder="Ingrese el nombre del grupo..."
+            fullWidth
+          />
+        </div>
+        <RHFListField<SkillCreate>
+          name="skills"
+          label="Habilidades"
+          StackComponent={SkillGroupSkillStack}
+          newItem={{
+            name: "",
+            level: LevelEnum.BASIC,
+          }}
+        />
+      </CardContent>
+    </Card>
+  );
+}
