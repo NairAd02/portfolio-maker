c31870ed5c22372565cad51f5d27b57f253d368c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-14 15:58:22 -0400

    feat(skill-groups): add PreviewImage component in SkillCard to display skill icons

--- src/sections/skill-groups/components/skill-card/skill-card.tsx
+++ src/sections/skill-groups/components/skill-card/skill-card.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { getLevelColor, getLevelText, Skill } from "@/lib/types/skill-groups";
 import SkillLevelStars from "../skill-level-starts/skill-level-starts";
 import { Badge } from "@/components/ui/badge";
+import PreviewImage from "@/components/preview-image/preview-image";
 
 interface Props {
   skill: Skill;
@@ -14,7 +15,14 @@ export default function SkillCard({ skill }: Props) {
       <CardContent className="p-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
-            {skill.icon && <span className="text-xl">{skill.icon}</span>}
+            {skill.icon && (
+              <PreviewImage
+                preview={skill.icon}
+                height={56}
+                width={56}
+                rounded={"2xl"}
+              />
+            )}
             <div>
               <h4 className="font-medium">{skill.name}</h4>
               <div className="flex items-center gap-2 mt-1">
