d79a376813080a1de7750b3208a529184e542809
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-14 17:26:24 -0400

    feat(certifications): add interfaces for certifications and certification details

---
+++ src/lib/types/certifications.ts
@@ -0,0 +1,19 @@
+export interface Certification {
+  id: string;
+  title: string;
+  institution: string;
+  startdate: string;
+  enddate: string;
+  image: string;
+  link: string;
+}
+
+export interface CertificationDetails {
+  id: string;
+  title: string;
+  institution: string;
+  startdate: string;
+  enddate: string;
+  image: string;
+  link: string;
+}
