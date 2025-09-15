50896f7d5be2809439110971200258731b39db1d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-14 21:58:51 -0400

    feat(certifications): add CertificationCreateDTO interface and function to convert certification creation data

--- src/lib/types/certifications.ts
+++ src/lib/types/certifications.ts
@@ -1,3 +1,5 @@
+import { CertificationCreate } from "@/sections/certifications/form/create/schemas/certification-create-schema";
+
 export interface Certification {
   id: string;
   title: string;
@@ -17,3 +19,22 @@ export interface CertificationDetails {
   image?: string;
   link?: string;
 }
+
+export interface CertificationCreateDTO {
+  title: string;
+  institution: string;
+  startdate: string;
+  enddate: string;
+  link?: string;
+}
+
+export const convertCertificationCreateDTO = (
+  certification: Omit<CertificationCreate, "image">
+): CertificationCreateDTO => {
+  return {
+    ...certification,
+    link: certification.link || undefined,
+    startdate: certification.startdate.toISOString(),
+    enddate: certification.enddate.toISOString(),
+  };
+};
