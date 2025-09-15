d5d9d146c93ac3560d3cb6e36851ca36e09c7fbc
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-14 21:55:06 -0400

    feat(certifications): add validation scheme for certification creation

---
+++ src/sections/certifications/form/create/schemas/certification-create-schema.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+export interface CertificationCreate {
+  title: string;
+  institution: string;
+  startdate: string;
+  enddate: string;
+  image?: string;
+  link: string;
+}
+
+export const certificationCreateSchema = z
+  .object({
+    title: z.string().min(1, { message: "El campo es requerido" }),
+    institution: z.string().min(1, { message: "El campo es requerido" }),
+    link: z.string(),
+    image: z
+      .instanceof(File, {
+        message: "Por favor selecciona una imagen.",
+      })
+      .optional()
+      .refine(
+        (file) => !file || file.size <= 5 * 1024 * 1024,
+        "La imagen no debe exceder 5MB."
+      )
+      .refine(
+        (file) => !file || file.type.startsWith("image/"),
+        "El archivo debe ser una imagen."
+      ),
+    startdate: z
+      .date()
+      .refine((date) => date <= new Date(), {
+        message: "La fecha no puede ser futura",
+      })
+      .refine((date) => date >= new Date("1900-01-01"), {
+        message: "La fecha es demasiado antigua",
+      }),
+    enddate: z
+      .date()
+      .refine((date) => date <= new Date(), {
+        message: "La fecha no puede ser futura",
+      })
+      .refine((date) => date >= new Date("1900-01-01"), {
+        message: "La fecha es demasiado antigua",
+      }),
+  })
+  .refine(
+    (data) => {
+      return data.startdate <= data.enddate;
+    },
+    {
+      message: "La fecha de inicio no puede ser mayor que la fecha de fin",
+      path: ["startdate"],
+    }
+  );
