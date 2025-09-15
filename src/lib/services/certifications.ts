3b34cabff8aa1c448cbe5f0c6d405114ee72b992
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-14 21:40:30 -0400

    feat(certifications): add function to create certifications and upload associated images

--- src/lib/services/certifications.ts
+++ src/lib/services/certifications.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { SupabaseClient } from "@supabase/supabase-js";
 import { createClient } from "../supabase/server";
-import { Certification } from "../types/certifications";
-import { getImageUrlOrThrow } from "./supabase-storage";
+import { Certification, CertificationCreateDTO } from "../types/certifications";
+import { getLoggedUser } from "./auth";
+import { getImageUrlOrThrow, uploadFileToSupabase } from "./supabase-storage";
+import { generateStorageFilePath } from "../images";
 
 export async function getCertificationsList() {
   const supabase = await createClient();
@@ -26,4 +29,77 @@ export async function getCertificationsList() {
   } catch (error) {
     return { data: null, error };
   }
+}
+
+export async function createCertification(
+  certificationCreateDTO: CertificationCreateDTO,
+  formData: FormData
+) {
+  const supabase = await createClient();
+
+  // get the session
+  const { data: sessionData, error: loggedUserError } = await getLoggedUser();
+
+  if (!sessionData || loggedUserError) return { data: null, loggedUserError };
+
+  const { data: portfolio, error: portfolioError } = await supabase
+    .from("portfolio")
+    .select("id")
+    .eq("user_id", sessionData.user.id)
+    .single();
+
+  if (portfolioError) return { data: null, error: portfolioError };
+
+  // insert the image
+  const { data: imageUploadData, error: imageUploadError } =
+    await insertCertificationImage(
+      supabase,
+      formData,
+      certificationCreateDTO.title
+    );
+
+  if (imageUploadError) return { data: null, error: imageUploadError };
+
+  const { data: createCertificationData, error: createCertificationError } =
+    await supabase
+      .from("certification")
+      .insert({
+        ...certificationCreateDTO,
+        image: imageUploadData,
+        portfolio_id: portfolio.id,
+      })
+      .select()
+      .single();
+
+  if (createCertificationError)
+    return { data: null, error: createCertificationError };
+
+  return { data: createCertificationData, error: null };
+}
+
+// aux functions
+async function insertCertificationImage(
+  supabase: SupabaseClient<any, "public", any>,
+  formData: FormData,
+  certificationName: string
+) {
+  const image = formData.get("image") as File;
+  if (!image) return { data: null, error: null };
+
+  const imagePath = generateStorageFilePath(
+    image,
+    `certifications/${certificationName}/image`
+  );
+
+  const uploadImageError = await uploadFileToSupabase(
+    supabase,
+    "portfolio-maker",
+    imagePath,
+    image,
+    "3600",
+    false
+  );
+  if (uploadImageError) return { data: null, error: uploadImageError };
+
+  return { data: imagePath, error: null };
 }
