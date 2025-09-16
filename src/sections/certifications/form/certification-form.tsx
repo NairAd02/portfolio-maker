1c48f9ad989fd816391d4caf88eff6d1fdc2486e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-16 10:20:57 -0400

    refactor(certifications): adjust the design of the certification form to improve usability and field presentation

--- src/sections/certifications/form/certification-form.tsx
+++ src/sections/certifications/form/certification-form.tsx
@@ -17,55 +17,57 @@ interface Props {
 
 export default function CertificationForm({ imageRecived }: Props) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <FileTextIcon className="w-5 h-5" />
-          Información General
-        </CardTitle>
-        <CardDescription>
-          Información general de la certificación
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-4">
-        <RHFImageUpload
-          name="image"
-          label="Imagen representativa de la certificación"
-          {...(imageRecived && { loading: imageRecived.loading })}
-        />
-        <div className="flex gap-6">
-          <RHFDatePickerField
-            name="startdate"
-            label="Fecha de Inicio *"
-            placeholder="Ingrese la fecha en la que empezó..."
+    <div className="flex-1 overflow-auto flex flex-col max-h-[75vh] p-2">
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2">
+            <FileTextIcon className="w-5 h-5" />
+            Información General
+          </CardTitle>
+          <CardDescription>
+            Información general de la certificación
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <RHFImageUpload
+            name="image"
+            label="Imagen representativa de la certificación"
+            {...(imageRecived && { loading: imageRecived.loading })}
           />
-          <RHFDatePickerField
-            name="enddate"
-            label="Fecha de Finalización"
-            placeholder="Ingrese la fecha en la que terminó..."
-          />
-        </div>
-        <div className="flex gap-6">
+          <div className="flex gap-6">
+            <RHFDatePickerField
+              name="startdate"
+              label="Fecha de Inicio *"
+              placeholder="Ingrese la fecha en la que empezó..."
+            />
+            <RHFDatePickerField
+              name="enddate"
+              label="Fecha de Finalización"
+              placeholder="Ingrese la fecha en la que terminó..."
+            />
+          </div>
+          <div className="flex gap-6">
+            <RHFTextField
+              name="title"
+              label="Título *"
+              placeholder="Ingrese el título de la certificación..."
+              fullWidth
+            />
+            <RHFTextField
+              name="institution"
+              label="Institución *"
+              placeholder="Ingrese la institución de la que adquirió la certificación..."
+              fullWidth
+            />
+          </div>
           <RHFTextField
-            name="title"
-            label="Título *"
-            placeholder="Ingrese el título de la certificación..."
+            name="link"
+            label="Enlace"
+            placeholder="Ingrese el enlace de la certificación..."
             fullWidth
           />
-          <RHFTextField
-            name="institution"
-            label="Institución *"
-            placeholder="Ingrese la institución de la que adquirió la certificación..."
-            fullWidth
-          />
-        </div>
-        <RHFTextField
-          name="link"
-          label="Enlace"
-          placeholder="Ingrese el enlace de la certificación..."
-          fullWidth
-        />
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
