932162f3f0da95c654d85a332c1bb71b8db54bcb
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-15 22:19:10 -0400

    feat(certifications): add a certification creation form with fields for images, dates, and details.

---
+++ src/sections/certifications/form/certification-form.tsx
@@ -0,0 +1,71 @@
+import { RHFDatePickerField } from "@/components/form/rhf-components/rhf-date-picker-field/rhf-date-picker-field";
+import { RHFImageUpload } from "@/components/form/rhf-components/rhf-image-upload/rhf-image-upload";
+import { RHFTextField } from "@/components/form/rhf-components/rhf-text-field/rhf-text-field";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { FileTextIcon } from "lucide-react";
+import React from "react";
+
+interface Props {
+  imageRecived?: { loading: boolean; error: string | null };
+}
+
+export default function CertificationForm({ imageRecived }: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          <FileTextIcon className="w-5 h-5" />
+          Información General
+        </CardTitle>
+        <CardDescription>
+          Información general de la certificación
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RHFImageUpload
+          name="image"
+          label="Imagen representativa de la certificación"
+          {...(imageRecived && { loading: imageRecived.loading })}
+        />
+        <div className="flex gap-6">
+          <RHFDatePickerField
+            name="startdate"
+            label="Fecha de Inicio *"
+            placeholder="Ingrese la fecha en la que empezó..."
+          />
+          <RHFDatePickerField
+            name="enddate"
+            label="Fecha de Finalización"
+            placeholder="Ingrese la fecha en la que terminó..."
+          />
+        </div>
+        <div className="flex gap-6">
+          <RHFTextField
+            name="title"
+            label="Título *"
+            placeholder="Ingrese el título de la certificación..."
+            fullWidth
+          />
+          <RHFTextField
+            name="institution"
+            label="Institución *"
+            placeholder="Ingrese la institución de la que adquirió la certificación..."
+            fullWidth
+          />
+          <RHFTextField
+            name="link"
+            label="Enlace"
+            placeholder="Ingrese el enlace de la certificación..."
+            fullWidth
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/sections/certifications/form/create/certification-create-form-container.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { paths } from "@/routes/path";
+import { revalidateServerPath } from "@/lib/cache";
+import FormActionButtons from "@/components/form/components/form-action-buttons";
+import { useRouter } from "next/navigation";
+import { AlertDestructive } from "@/components/ui/alert-destructive";
+import useCreateCertification from "../../hooks/use-create-certification";
+import {
+  CertificationCreate,
+  certificationCreateSchema,
+} from "./schemas/certification-create-schema";
+import CertificationForm from "../certification-form";
+
+export default function CertificationCreateFormContainer() {
+  const router = useRouter();
+  const {
+    loading: submitLoading,
+    createCertification,
+    error: createCertificationError,
+  } = useCreateCertification({
+    onCreateAction: () => {
+      toast.success("Certificación creada con éxito");
+      revalidateServerPath(paths.certifications.root);
+      handleClose();
+      router.refresh();
+    },
+  });
+  const form = useForm<CertificationCreate>({
+    resolver: zodResolver(certificationCreateSchema),
+    defaultValues: {
+      title: "",
+      institution: "",
+      link: "",
+    },
+  });
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  function onSubmit(certification: CertificationCreate) {
+    createCertification(certification);
+  }
+  return (
+    <FormProvider {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
+      >
+        {createCertificationError && (
+          <AlertDestructive title={createCertificationError} />
+        )}
+        <CertificationForm />
+        <FormActionButtons
+          submitLoading={submitLoading}
+          submitButtonText="Crear Certificación"
+          handleClose={handleClose}
+        />
+      </form>
+    </FormProvider>
+  );
+}
