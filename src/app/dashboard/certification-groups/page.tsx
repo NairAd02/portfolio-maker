da1bbbff1535d21ab275e64288b2f14a08948ffc
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-17 18:32:10 -0400

    feat(certifications): add a modal to select certifications on the certification groups page

--- src/app/dashboard/certification-groups/page.tsx
+++ src/app/dashboard/certification-groups/page.tsx
@@ -1,10 +1,16 @@
+import Modal from "@/components/modal/modal";
+import { modalTypes } from "@/components/modal/types/modalTypes";
 import CertificationGroupsContainer from "@/sections/certification-groups/certification-groups-container";
+import SelectableCertificationsModalContainer from "@/sections/certifications/components/selectable-certifications/selectable-certifications-modal-container";
 import React from "react";
 
 export default function CertificationGroupsPage() {
   return (
     <>
       <CertificationGroupsContainer />
+      <Modal formPath={modalTypes.selectableCertificationsModal.name}>
+        <SelectableCertificationsModalContainer />
+      </Modal>
     </>
   );
 }
