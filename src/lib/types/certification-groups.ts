374817ea3e43bded632a19838d26802b32b32d5d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-17 16:36:26 -0400

    feat(certifications): add DTO and function to convert certification group creation data

--- src/lib/types/certification-groups.ts
+++ src/lib/types/certification-groups.ts
@@ -1,3 +1,4 @@
+import { CertificationGroupCreate } from "@/sections/certification-groups/form/create/schemas/certification-group-create-schema";
 import { Certification } from "./certifications";
 
 export interface CertificationGroup {
@@ -9,3 +10,16 @@ export interface CertificationGroupDetails {
   title: string;
   certifications: Certification[];
 }
+
+export interface CertificationGroupCreateDTO {
+  title: string;
+  certifications: string[]; // id of certifications
+}
+
+export const convertCertificationGroupCreateDTO = (
+  certificationGroup: CertificationGroupCreate
+): CertificationGroupCreateDTO => {
+  return {
+    ...certificationGroup,
+  };
+};
