7b5b73d267b99774be05ca4bef1538f19ab6540f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-17 18:26:48 -0400

    feat(certifications): add a form to create certification groups and their container

---
+++ src/sections/certification-groups/form/certification-group-form.tsx
@@ -0,0 +1,46 @@
+"use client";
+import RHFSelectableCardListField from "@/components/form/rhf-components/rhf-selectable-card-list-field/rhf-selectable-card-list-field";
+import { RHFTextField } from "@/components/form/rhf-components/rhf-text-field/rhf-text-field";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import CertificationCard from "@/sections/certifications/components/certification-card/certification-card";
+import { CertificationSchema } from "@/sections/certifications/form/schemas/certification-schema";
+import { FileTextIcon } from "lucide-react";
+import React from "react";
+
+export default function CertificationGroupForm() {
+  return (
+    <div className="flex-1 overflow-auto flex flex-col max-h-[75vh] p-2">
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2">
+            <FileTextIcon className="w-5 h-5" />
+            Información General
+          </CardTitle>
+          <CardDescription>
+            Información general de la certificación
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <RHFTextField
+            name="title"
+            label="Título *"
+            placeholder="Ingrese el título del grupo de certificaciones..."
+            fullWidth
+          />
+          <RHFSelectableCardListField<CertificationSchema>
+            name="certifications"
+            CardComponent={CertificationCard}
+            modalPath={modalTypes.selectableCertificationsModal.name}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ src/sections/certification-groups/form/create/certification-group-create-form-container.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { paths } from "@/routes/path";
+import { revalidateServerPath } from "@/lib/cache";
+import FormActionButtons from "@/components/form/components/form-action-buttons";
+import { useRouter } from "next/navigation";
+import { AlertDestructive } from "@/components/ui/alert-destructive";
+import useCreateCertificationGroup from "../../hooks/use-create-certification-group";
+import {
+  CertificationGroupCreate,
+  certificationGroupCreateSchema,
+} from "./schemas/certification-group-create-schema";
+import CertificationGroupForm from "../certification-group-form";
+
+export default function CertificationGroupCreateFormContainer() {
+  const router = useRouter();
+  const {
+    loading: submitLoading,
+    createCertificationGroup,
+    error: createCertificationGroupError,
+  } = useCreateCertificationGroup({
+    onCreateAction: () => {
+      toast.success("Grupo de certificaciones creado con éxito");
+      revalidateServerPath(paths.certificationGroups.root);
+      handleClose();
+      router.refresh();
+    },
+  });
+  const form = useForm<CertificationGroupCreate>({
+    resolver: zodResolver(certificationGroupCreateSchema),
+    defaultValues: {
+      title: "",
+      certifications: [],
+    },
+  });
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  function onSubmit(certificationGroup: CertificationGroupCreate) {
+    createCertificationGroup(certificationGroup);
+  }
+  return (
+    <FormProvider {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
+      >
+        {createCertificationGroupError && (
+          <AlertDestructive title={createCertificationGroupError} />
+        )}
+        <CertificationGroupForm />
+        <FormActionButtons
+          submitLoading={submitLoading}
+          submitButtonText="Crear Grupo de Certificaciones"
+          handleClose={handleClose}
+        />
+      </form>
+    </FormProvider>
+  );
+}
