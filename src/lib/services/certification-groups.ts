840eea793e3b08413f602593fe1a8da96e9587c7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-18 10:24:23 -0400

    feat(certification-groups): add function to edit certification groups and manage associated certifications

--- src/lib/services/certification-groups.ts
+++ src/lib/services/certification-groups.ts
@@ -4,6 +4,7 @@ import { createClient } from "../supabase/server";
 import {
   CertificationGroup,
   CertificationGroupCreateDTO,
+  CertificationGroupEditDTO,
 } from "../types/certification-groups";
 import { getLoggedUser } from "./auth";
 import { insertCertificationsGroups } from "./certifications";
@@ -86,4 +87,58 @@ export async function createCertificationGroup(
   }
 
   return { data: createCertificationGroupData, error: null };
+}
+
+export async function editCertificationGroup(
+  id: string,
+  certificationGroupEditDTO: CertificationGroupEditDTO
+) {
+  const { certifications, ...restCertificationGroupEditDTO } =
+    certificationGroupEditDTO;
+  const supabase = await createClient();
+
+  // find the certification group to edit
+  const { error: certificationGroupFindError } = await supabase
+    .from("certificationgroup")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (certificationGroupFindError)
+    return { data: null, error: certificationGroupFindError };
+
+  const {
+    data: updateCertificationGroupData,
+    error: updateCertificationGroupError,
+  } = await supabase
+    .from("certificationgroup")
+    .update({
+      ...restCertificationGroupEditDTO,
+    })
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (updateCertificationGroupError)
+    return { data: null, error: updateCertificationGroupError };
+
+  // update the certifications
+
+  // first delete current certifications
+  const { error: deleteCertifications } = await supabase
+    .from("certification_has_certificationgroup")
+    .delete()
+    .eq("certification_group_id", id);
+
+  if (deleteCertifications) return { data: null, error: deleteCertifications };
+
+  // insert new certifications
+  if (certifications && certifications.length > 0) {
+    const { error: insertCertificationsError } =
+      await insertCertificationsGroups(supabase, id, certifications);
+    if (insertCertificationsError)
+      return { data: null, error: insertCertificationsError };
+  }
+
+  return { data: updateCertificationGroupData, error: null };
 }
