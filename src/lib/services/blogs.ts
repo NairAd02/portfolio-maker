14e62f763a11bce18ffb3e42bc45bb24ad0b36a9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-18 18:18:27 -0400

    feat(blogs): add function to create a blog and get the logged in user

--- src/lib/services/blogs.ts
+++ src/lib/services/blogs.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { createClient } from "../supabase/server";
-import { Blog } from "../types/blogs";
+import { Blog, BlogCreateDTO } from "../types/blogs";
+import { getLoggedUser } from "./auth";
 
 export async function getBlogsList() {
   const supabase = await createClient();
@@ -9,4 +10,34 @@ export async function getBlogsList() {
   const blogs = blogsData as Blog[];
 
   return { data: blogs, error };
+}
+
+export async function createBlog(blogCreateDTO: BlogCreateDTO) {
+  const supabase = await createClient();
+
+  // get the session
+  const { data: sessionData, error: loggedUserError } = await getLoggedUser();
+
+  if (!sessionData || loggedUserError) return { data: null, loggedUserError };
+
+  const { data: portfolio, error: portfolioError } = await supabase
+    .from("portfolio")
+    .select("id")
+    .eq("user_id", sessionData.user.id)
+    .single();
+
+  if (portfolioError) return { data: null, error: portfolioError };
+
+  const { data: createBlogData, error: createBlogError } = await supabase
+    .from("blog")
+    .insert({
+      ...blogCreateDTO,
+      portfolio_id: portfolio.id,
+    })
+    .select()
+    .single();
+
+  if (createBlogError) return { data: null, error: createBlogError };
+
+  return { data: createBlogData, error: null };
 }
