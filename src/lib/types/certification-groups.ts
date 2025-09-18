57fdf4e846be9b7209781cbfb0854911b647f77a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-18 10:29:09 -0400

    feat(certification-groups): add DTO to edit certification groups and corresponding conversion function

--- src/lib/types/certification-groups.ts
+++ src/lib/types/certification-groups.ts
@@ -1,5 +1,6 @@
 import { CertificationGroupCreate } from "@/sections/certification-groups/form/create/schemas/certification-group-create-schema";
 import { Certification } from "./certifications";
+import { CertificationGroupEdit } from "@/sections/certification-groups/form/edit/schemas/certification-group-edit-schema";
 
 export interface CertificationGroup {
   title: string;
@@ -16,9 +17,25 @@ export interface CertificationGroupCreateDTO {
   certifications: string[]; // id of certifications
 }
 
+export interface CertificationGroupEditDTO {
+  title: string;
+  certifications: string[]; // id of certifications
+}
+
 export const convertCertificationGroupCreateDTO = (
   certificationGroup: CertificationGroupCreate
 ): CertificationGroupCreateDTO => {
+  return {
+    ...certificationGroup,
+    certifications: certificationGroup.certifications.map((certification) =>
+      String(certification.id)
+    ),
+  };
+};
+
+export const convertCertificationGroupEditDTO = (
+  certificationGroup: CertificationGroupEdit
+): CertificationGroupEditDTO => {
   return {
     ...certificationGroup,
     certifications: certificationGroup.certifications.map((certification) =>
