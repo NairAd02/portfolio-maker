7fddf3c240f31187fc12fb15826d9a87d08d2445
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-18 11:37:12 -0400

    feat(certification-groups): create a container for deleting certification groups with confirmation and error handling

---
+++ src/sections/certification-groups/delete/certification-group-delete-container.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { useCallback } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { paths } from "@/routes/path";
+import { revalidateServerPath } from "@/lib/cache";
+import ConfirmationPanel from "@/components/confirmation-panel/confirmation-panel";
+import useDeleteCertificationGroup from "../hooks/use-delete-certification-group";
+
+interface Props {
+  id: string;
+}
+
+export default function CertificationGroupDeleteContainer({ id }: Props) {
+  const router = useRouter();
+  const { deleteCertificationGroup, loading, error } =
+    useDeleteCertificationGroup({
+      id,
+      onDeleteAction: () => {
+        toast.success("Grupo de Certificaciones eliminado con éxito");
+        revalidateServerPath(paths.certificationGroups.root);
+        handleClose();
+        router.refresh();
+      },
+    });
+  const handleClose = useCallback(() => {
+    router.back();
+  }, [router]);
+  return (
+    <ConfirmationPanel
+      title={"Eliminación del Grupo de Certificaciones"}
+      message={"¿Seguro que desea eliminar el grupo de certificaciones?"}
+      warningMessage={
+        "La eliminación del grupo de certificaciones es irreversible, este cambio no se puede deshacer"
+      }
+      onConfirm={deleteCertificationGroup}
+      onCancel={handleClose}
+      isLoading={loading}
+      isDestructive
+      error={error}
+    />
+  );
+}
