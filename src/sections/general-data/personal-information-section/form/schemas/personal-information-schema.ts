4f2ec57d3ce909886d87431adb62235b0ad82459
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-20 16:30:13 -0400

    feat(personal-information): adding a validation scheme for personal information using Zod

---
+++ src/sections/general-data/personal-information-section/form/schemas/personal-information-schema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export interface PersonalInformationSchema {
+  contact_name: string;
+  introductory_phrase: string;
+  contact_image: File;
+}
+
+export const personalInformationSchema = z.object({
+  contact_name: z.string().min(1, { message: "El campo es obligatorio" }),
+  introductory_phrase: z
+    .string()
+    .min(1, { message: "El campo es obligatorio" }),
+  contact_image: z
+    .instanceof(File, {
+      message: "Por favor selecciona una imagen.",
+    })
+    .optional()
+    .refine(
+      (file) => !file || file.size <= 5 * 1024 * 1024,
+      "La imagen no debe exceder 5MB."
+    )
+    .refine(
+      (file) => !file || file.type.startsWith("image/"),
+      "El archivo debe ser una imagen."
+    ),
+});
