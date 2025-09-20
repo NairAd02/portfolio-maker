deac4b350b14817116ba879420d47331596c26c1
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-20 14:23:04 -0400

    feat(portfolio): add Portfolio interface to structure portfolio data

---
+++ src/lib/types/portfolio.ts
@@ -0,0 +1,13 @@
+export interface Portfolio {
+  id: string;
+  contact_name: string;
+  contact_image: string;
+  introductory_phrase: string;
+  about_text: string;
+  feature_projecttext: string;
+  work_experiencetext: string;
+  technologies_and_skills_text: string;
+  education_and_certifications_text: string;
+  blog_and_post_text: string;
+  contact_text: string;
+}
