9106f08fb2db54f37f5ec6fb07b8a541fba91161
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-20 17:26:38 -0400

    feat(portfolio): add the PersonalInformationReport interface and make the contact image field optional in Portfolio

--- src/lib/types/portfolio.ts
+++ src/lib/types/portfolio.ts
@@ -1,9 +1,9 @@
-import { PersonalInformationSchema } from "@/sections/general-data/personal-information-section/form/schemas/personal-information-schema";
+import { PersonalInformationSchema } from "@/sections/general-data/personal-information/form/schemas/personal-information-schema";
 
 export interface Portfolio {
   id: string;
   contact_name: string;
-  contact_image: string;
+  contact_image?: string;
   introductory_phrase: string;
   about_text: string;
   feature_projecttext: string;
@@ -12,6 +12,14 @@ export interface Portfolio {
   education_and_certifications_text: string;
   blog_and_post_text: string;
   contact_text: string;
+}
+
+// PersonalInformationReport
+export interface PersonalInformationReport {
+  portfolioId: string;
+  contact_name: string;
+  contact_image?: string;
+  introductory_phrase: string;
 }
 
 export interface PersonalInformationDTO {
