85700075b590841ad65205a37965e967fb219a1e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-25 21:40:51 -0400

    feat(certificaciones): add function to edit the certifications section in the portfolio

--- src/lib/services/portfolio.ts
+++ src/lib/services/portfolio.ts
@@ -4,6 +4,7 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import { generateStorageFilePath } from "../images";
 import { createClient } from "../supabase/server";
 import {
+  CertificationsSectionDTO,
   CertificationsSectionReport,
   ExperiencesSectionDTO,
   ExperiencesSectionReport,
@@ -343,6 +344,40 @@ export async function editSkillsSection(skillsSectionDTO: SkillsSectionDTO) {
       .from("portfolio")
       .update({
         ...skillsSectionDTO,
+      })
+      .eq("id", portfolioEntity.id)
+      .select()
+      .single();
+  if (updatePortfolioError) return { data: null, error: updatePortfolioError };
+
+  return { data: updatePortfolioData, error: null };
+}
+
+export async function editCertificationsSection(
+  certificationsSectionDTO: CertificationsSectionDTO
+) {
+  const supabase = await createClient();
+
+  // get the session
+  const { data: sessionData, error: loggedUserError } = await getLoggedUser();
+
+  if (!sessionData || loggedUserError) return { data: null, loggedUserError };
+
+  const { data: portfolio, error: portfolioError } = await supabase
+    .from("portfolio")
+    .select("id")
+    .eq("user_id", sessionData.user.id)
+    .single();
+
+  if (portfolioError) return { data: null, error: portfolioError };
+
+  const portfolioEntity = portfolio as Portfolio;
+
+  const { data: updatePortfolioData, error: updatePortfolioError } =
+    await supabase
+      .from("portfolio")
+      .update({
+        ...certificationsSectionDTO,
       })
       .eq("id", portfolioEntity.id)
       .select()
