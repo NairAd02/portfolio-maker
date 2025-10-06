76e64ac823c211ed5e502cbcb08192d597eb32d3
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-06 14:39:01 -0400

    refactor(sidebar-header-content): adjust the spacing and class of the icon in the sidebar header component

--- src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
+++ src/sections/dashboard-layout/components/app-sidebar/sidebar-header-content/sidebar-header-content.tsx
@@ -6,8 +6,8 @@ import React from "react";
 export default function SidebarHeaderContent() {
   const { open } = useSidebar();
   return (
-    <div className="flex text-white flex-col gap-4 items-center justify-center">
-      <BriefcaseBusiness className={` ${open ? "size-12" : "size-8"}`} />
+    <div className="flex text-white gap-2 items-center justify-center">
+      <BriefcaseBusiness className={` ${open ? "size-8" : "size-8"}`} />
       {open && <p className="text-lg font-bold">Maker Panel</p>}
     </div>
   );
