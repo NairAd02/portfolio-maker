290ef110e1447ed8b425a3484c1362ddcfc10504
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-06 14:11:01 -0400

    refactor(avatar-container): add optional className property to AvatarContainer component

--- src/components/ui/avatar-container.tsx
+++ src/components/ui/avatar-container.tsx
@@ -4,11 +4,12 @@ import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 interface Props {
   image?: string;
   fallback: string;
+  className?: string;
 }
 
-export default function AvatarContainer({ image, fallback }: Props) {
+export default function AvatarContainer({ image, fallback, className }: Props) {
   return (
-    <Avatar>
+    <Avatar className={className}>
       {image && <AvatarImage src={image} />}
       <AvatarFallback>{fallback}</AvatarFallback>
     </Avatar>
