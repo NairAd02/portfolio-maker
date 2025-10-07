0b5564007172318e4a32fde92f35511d38b3cebf
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-07 09:05:27 -0400

    refactor(create-project-page): delete the file from the project creation page in the modal

--- src/app/@modal/(projects)/create-project/page.tsx
+++
@@ -1,6 +0,0 @@
-import ProjectCreateFormContainer from "@/sections/projects/form/new/project-create-form-containter";
-import React from "react";
-
-export default function CreateProyectPage() {
-  return <ProjectCreateFormContainer />;
-}
