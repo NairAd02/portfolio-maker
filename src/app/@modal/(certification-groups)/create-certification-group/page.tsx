f7eae741a8ce99c8b41c2ae2e749db159ae06d5c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-07 22:53:04 -0400

    refactor(@modal): remove previous implementation of certification-groups modal

--- src/app/@modal/(certification-groups)/create-certification-group/page.tsx
+++
@@ -1,6 +0,0 @@
-import CertificationGroupCreateFormContainer from "@/sections/certification-groups/form/create/certification-group-create-form-container";
-import React from "react";
-
-export default function CreateCertificationGroupPage() {
-  return <CertificationGroupCreateFormContainer />;
-}
