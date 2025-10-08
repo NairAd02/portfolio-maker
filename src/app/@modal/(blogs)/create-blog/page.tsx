4d77b6c02c66ad484fc62c17f660606207ee8313
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-07 22:41:41 -0400

    refactor(@modal): remove previous implementation of blog modal

--- src/app/@modal/(blogs)/create-blog/page.tsx
+++
@@ -1,6 +0,0 @@
-import BlogCreateFormContainer from "@/sections/blogs/form/create/blog-create-form-container";
-import React from "react";
-
-export default function CreateBlogPage() {
-  return <BlogCreateFormContainer />;
-}
