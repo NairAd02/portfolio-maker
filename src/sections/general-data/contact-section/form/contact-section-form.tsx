4abbeb6bbbb167a8bf6daf322bfaf26c8e80000e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-09 10:51:31 -0400

    feat(contact-section): replace RHFTextAreaField with RHFTextField for contact email input in the contact section form

--- src/sections/general-data/contact-section/form/contact-section-form.tsx
+++ src/sections/general-data/contact-section/form/contact-section-form.tsx
@@ -1,10 +1,11 @@
 import { RHFTextAreaField } from "@/components/form/rhf-components/rhf-text-area-field/rhf-text-area-field";
+import { RHFTextField } from "@/components/form/rhf-components/rhf-text-field/rhf-text-field";
 import React from "react";
 
 export default function ContactSectionForm() {
   return (
     <div className="flex items-center justify-center gap-6">
-      <RHFTextAreaField
+      <RHFTextField
         name="contact_email"
         label="Email de contacto"
         placeholder="tu@email.com"
