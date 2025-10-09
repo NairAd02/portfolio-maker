bf4894b7046c5093a1c0d85a2bbbd7b7b9e24c2e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-08 21:16:57 -0400

    refactor(@modal): remove previous implementation of certifications modal

--- src/app/@modal/(certifications)/create-certification/page.tsx
+++
@@ -1,6 +0,0 @@
-import CertificationCreateFormContainer from "@/sections/certifications/form/create/certification-create-form-container";
-import React from "react";
-
-export default function CreateCertificationPage() {
-  return <CertificationCreateFormContainer />;
-}
