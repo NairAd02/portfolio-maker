4ca185563e4e649e0eba8591cbc7117117abb1a9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-08 21:29:44 -0400

    refactor(@modal): remove previous implementation of experiences modal

--- src/app/@modal/(experiences)/create-experience/page.tsx
+++
@@ -1,6 +0,0 @@
-import ExperienceCreateFormContainer from "@/sections/experiences/form/create/experience-create-form-container";
-import React from "react";
-
-export default function CreateExperience() {
-  return <ExperienceCreateFormContainer />;
-}
