67291ac9cf2d71a2c7c1d42ca3b416efa1e8eb7a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-08 21:42:10 -0400

    refactor(@modal): remove previous implementation of skill-groups modal

--- src/app/@modal/(skill-groups)/create-skill-group/page.tsx
+++
@@ -1,6 +0,0 @@
-import SkillGroupCreateFormContainer from "@/sections/skill-groups/form/create/skill-group-create-form-container";
-import React from "react";
-
-export default function CreateSkillGroupPage() {
-  return <SkillGroupCreateFormContainer />;
-}
