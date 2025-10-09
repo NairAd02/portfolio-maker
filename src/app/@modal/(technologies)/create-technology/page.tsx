1f1ed6a546eeb48c588142f7f748f2880bb512b3
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-08 21:53:18 -0400

    refactor(@modal): remove previous implementation of technologies modal

--- src/app/@modal/(technologies)/create-technology/page.tsx
+++
@@ -1,6 +0,0 @@
-import TechnologyCreateFormContainer from "@/sections/technologies/form/create/technology-create-form-container";
-import React from "react";
-
-export default function CreateTechnologyPage() {
-  return <TechnologyCreateFormContainer />;
-}
