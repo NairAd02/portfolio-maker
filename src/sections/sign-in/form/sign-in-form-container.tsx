0153edd2fc95dc50ff1fa09fdca526540fd037fd
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-25 16:24:48 -0400

    refactor(sign-in-form-container): replace router navigation with window.location for sign-in success and clean up imports

--- src/sections/sign-in/form/sign-in-form-container.tsx
+++ src/sections/sign-in/form/sign-in-form-container.tsx
@@ -7,14 +7,10 @@ import { Button } from "@/components/ui/button";
 import SignInForm from "./sign-in-form";
 import useSignIn from "./hooks/use-sign-in";
 import { toast } from "react-toastify";
-import { useRouter } from "next/navigation";
 import { paths } from "@/routes/path";
 import { AlertDestructive } from "@/components/ui/alert-destructive";
 
-
-
 export default function SignInFormContainer() {
-  const router = useRouter();
   const {
     signIn,
     loading: submitLoading,
@@ -22,7 +18,7 @@ export default function SignInFormContainer() {
   } = useSignIn({
     onSignInAction: () => {
       toast.success("Inicio de sessión realizado con éxtio");
-      router.push(paths.projects.root);
+      window.location.href = paths.generalData.root;
     },
   });
   const form = useForm<Credentials>({
@@ -40,9 +36,7 @@ export default function SignInFormContainer() {
   return (
     <FormProvider {...form}>
       <div className="flex-grow flex flex-col gap-4 justify-center max-w-md mx-auto w-full">
-        <h1 className="text-3xl font-bold">
-          Inicia sesión
-        </h1>
+        <h1 className="text-3xl font-bold">Inicia sesión</h1>
         <p className="text-gray-500">
           Ingrese el correo electrónico asociado a su cuenta
         </p>
