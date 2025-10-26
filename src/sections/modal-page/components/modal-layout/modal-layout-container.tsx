566a5e0d2fb44da9f02753ec042030a6f9574dff
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-25 22:28:14 -0400

    feat(modal): add optional modalClassName prop to ModalLayoutContainer and ModalWrapper for customizable styling

--- src/sections/modal-page/components/modal-layout/modal-layout-container.tsx
+++ src/sections/modal-page/components/modal-layout/modal-layout-container.tsx
@@ -5,21 +5,25 @@ import ExitModalButton from "../exit-modal-button/exit-modal-button";
 
 interface Props {
   children: ReactNode;
+  modalClassName?: string;
 }
 
-export default function ModalLayoutContainer({ children }: Props) {
-  const maxWidth = "max-w-2xl";
-
+export default function ModalLayoutContainer({
+  children,
+  modalClassName = "max-w-2xl",
+}: Props) {
   return (
     <div
-      className={`flex flex-col w-full max-h-[98vh] ${maxWidth} gap-4 bg-background rounded-2xl shadow-xl`}
+      className={`flex flex-col w-full max-h-[98vh] gap-4 bg-background rounded-2xl shadow-xl ${modalClassName}`}
     >
       {/* Botón de cerrar */}
       <div className="flex w-full p-1 bg-primary gap-2 justify-between rounded-t-2xl items-center">
         <p></p>
         <ExitModalButton />
       </div>
-      <div className="flex-1 pl-4 pr-2 pb-2 rounded-2xl mr-2 mb-2 overflow-auto">{children}</div>
+      <div className="flex-1 pl-4 pr-2 pb-2 rounded-2xl mr-2 mb-2 overflow-auto">
+        {children}
+      </div>
     </div>
   );
 }
--- src/sections/modal-page/components/modal-wrapper/modal-wrapper.tsx
+++ src/sections/modal-page/components/modal-wrapper/modal-wrapper.tsx
@@ -4,6 +4,7 @@ export default function ModalWrapper({
   children,
 }: {
   children: React.ReactNode;
+  modalClassName?: string
 }) {
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
