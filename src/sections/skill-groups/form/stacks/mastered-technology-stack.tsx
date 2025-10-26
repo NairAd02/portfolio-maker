ac0a4abbb54eb261d028c61e78b2ac9f0b539c90
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-25 21:44:54 -0400

    feat(skill-groups): create SkillGroupSkillStack component for selecting mastered technologies and levels

---
+++ src/sections/skill-groups/form/stacks/mastered-technology-stack.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { RHFSelectField } from "@/components/form/rhf-components/rhf-select-field/rhf-select-field";
+import { getLevelText, LevelEnum } from "@/lib/types/skill-groups";
+import useTechnologies from "@/sections/technologies/hooks/use-technologies";
+import React from "react";
+
+interface Props {
+  index: number;
+}
+
+export default function SkillGroupSkillStack({ index }: Props) {
+  const { technologies, loadingData: loadingDataTechnologies } =
+    useTechnologies();
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-4">
+        <RHFSelectField
+          name={`masteredTechnologies.${index}.technologyId`}
+          options={technologies.map((tech) => ({
+            label: tech.name,
+            value: tech.id,
+          }))}
+          loading={loadingDataTechnologies}
+          placeholder="Introduzca la tecnología dominada..."
+          fullWidth
+        />
+        <RHFSelectField
+          name={`masteredTechnologies.${index}.level`}
+          options={[
+            {
+              label: getLevelText(LevelEnum.ADVANCED),
+              value: LevelEnum.ADVANCED,
+            },
+            {
+              label: getLevelText(LevelEnum.INTERMEDIATE),
+              value: LevelEnum.INTERMEDIATE,
+            },
+            {
+              label: getLevelText(LevelEnum.BASIC),
+              value: LevelEnum.BASIC,
+            },
+          ]}
+          placeholder="Introduzca el nivel de dominio de la tecnología..."
+          fullWidth
+        />
+      </div>
+    </div>
+  );
+}
