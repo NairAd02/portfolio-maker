b69985a7d5d3c5f32f86a02a33037371057aa742
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-28 21:26:03 -0500

    refactor(supabase): simplify client creation and enhance cookie handling in server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,28 +1,29 @@
 import { createServerClient } from "@supabase/ssr";
-import { SupabaseClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 
-let client: SupabaseClient<any, "public", any> | null = null;
-
 export async function createClient() {
-  if (client) return client;
-
   const cookieStore = await cookies();
-  client = createServerClient(
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll: () => cookieStore.getAll(),
-        setAll: (cookiesToSet) => {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             );
-          } catch {}
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
     }
   );
-  return client;
 }
